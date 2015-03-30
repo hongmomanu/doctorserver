87930ef287bada113df44f4e8ff4a105236a4c48
jack <hongmomanu@163.com>
2015-03-30 09:11:45 +0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/doctorserver.jar clojure.main -m doctorserver.core
---
+++ project.clj
@@ -0,0 +1,66 @@
+(defproject doctorserver "0.1.0-SNAPSHOT"
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring-server "0.4.0"]
+                 [selmer "0.8.2"]
+                 [com.taoensso/timbre "3.4.0"]
+                 [com.taoensso/tower "3.0.2"]
+                 [markdown-clj "0.9.65"]
+                 [environ "1.0.0"]
+                 [im.chit/cronj "1.4.3"]
+                 [compojure "1.3.2"]
+                 [ring/ring-defaults "0.1.4"]
+                 [ring/ring-session-timeout "0.1.0"]
+                 [ring-middleware-format "0.5.0"]
+                 [noir-exception "0.2.3"]
+                 [bouncer "0.3.2"]
+                 [prone "0.8.1"]
+                 [ragtime "0.3.8"]
+                 [yesql "0.5.0-rc1"]
+                 [com.h2database/h2 "1.4.182"]]
+
+  :min-lein-version "2.0.0"
+  :uberjar-name "doctorserver.jar"
+  :repl-options {:init-ns doctorserver.handler}
+  :jvm-opts ["-server"]
+
+  :main doctorserver.core
+
+  :plugins [[lein-ring "0.9.1"]
+            [lein-environ "1.0.0"]
+            [lein-ancient "0.6.5"]
+            [ragtime/ragtime.lein "0.3.8"]]
+  
+
+  :ring {:handler doctorserver.handler/app
+         :init    doctorserver.handler/init
+         :destroy doctorserver.handler/destroy
+         :uberwar-name "doctorserver.war"}
+  
+  :ragtime
+  {:migrations ragtime.sql.files/migrations
+   :database "jdbc:h2:./site.db"}
+  
+  
+  
+  
+  :profiles
+  {:uberjar {:omit-source true
+             :env {:production true}
+            
+             :aot :all}
+   :dev {:dependencies [[ring-mock "0.1.5"]
+                        [ring/ring-devel "1.3.2"]
+                        [pjstadig/humane-test-output "0.7.0"]
+                        ]
+         :source-paths ["env/dev/clj"]
+         
+        
+         
+         :repl-options {:init-ns doctorserver.repl}
+         :injections [(require 'pjstadig.humane-test-output)
+                      (pjstadig.humane-test-output/activate!)]
+         :env {:dev true}}})
