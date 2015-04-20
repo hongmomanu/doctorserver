aba20902543dc6b378f4b1462a2902c446b234dc
jack <hongmomanu@163.com>
2015-04-20 17:07:29 +0800

    jack commit

--- src/doctorserver/routes/home.clj
+++ src/doctorserver/routes/home.clj
@@ -1,8 +1,12 @@
 (ns doctorserver.routes.home
   (:require [doctorserver.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.response :as resp]
             [clojure.java.io :as io]
+            [noir.io :as nio]
+            [clj-time.coerce :as c]
+            [clj-time.local :as l]
+            [noir.response :as nresp]
 
             ))
 
@@ -19,4 +23,23 @@
     (GET "/downloadtest/*" [] (resp/redirect "/cordova-app-hello-world-3.6.3.tar.gz"))
 
 
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+
+  (POST "/common/uploadfile"  [file]
+
+    (println "file up loaddd")
+
+    (let [
+           datapath (str (System/getProperty "user.dir") "/")
+           uploadpath (str datapath "upload/")
+          timenow (c/to-long  (l/local-now))
+          filename (str timenow (:filename file))
+          ]
+      (nio/upload-file uploadpath  (conj file {:filename filename}))
+      (nresp/json {:success true :filename (:filename file)})
+      )
+
+    )
+
+
+  )
