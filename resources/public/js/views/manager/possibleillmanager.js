4544394a10b9a1b1f778725ef426b3a969b1a076
jack <hongmomanu@163.com>
2015-05-14 13:19:27 +0800

    jack commit

---
+++ resources/public/js/views/manager/possibleillmanager.js
@@ -0,0 +1,42 @@
+/**
+ * Created by jack on 5/14/15.
+ */
+define(function () {
+
+    function render(parameters) {
+        $('#possibleillmanagerpanel').datagrid({
+            singleSelect: true,
+            collapsible: true,
+            rownumbers: true,
+            method:'post',
+            fitColumns:true,
+            url:'../hospital/getills',
+            remoteSort: false,
+            /*sortName:'time',
+             sortOrder:'desc',*/
+            fit:true,
+            //toolbar:'#packagepaneltb',
+            pagination:true,
+            pageSize:10,
+
+
+            toolbar:'#packagepaneldetailtb',
+            onBeforeLoad: function (params) {
+                //alert(1);
+                var options = $('#possibleillmanagerpanel').datagrid('options');
+                params.start = (options.pageNumber - 1) * options.pageSize;
+                params.limit = options.pageSize;
+                params.totalname = "total";
+                params.rowsname = "rows";
+            }/*,
+            onClickRow:onClickRow*/
+
+        });
+
+    }
+
+    return {
+        render: render
+
+    };
+});
