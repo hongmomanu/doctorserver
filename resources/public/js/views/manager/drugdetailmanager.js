a91209c88090bab1d3795369d2cc4df660ce7aef
jack <hongmomanu@163.com>
2015-05-19 16:28:16 +0800

    jack commit

--- resources/public/js/views/manager/drugdetailmanager.js
+++ resources/public/js/views/manager/drugdetailmanager.js
@@ -6,57 +6,15 @@ define(function () {
     function render(parameters) {
 
 
-        $.extend($.fn.datagrid.methods, {
-            editCell: function(jq,param){
-                return jq.each(function(){
-                    var opts = $(this).datagrid('options');
-                    var fields = $(this).datagrid('getColumnFields',true).concat($(this).datagrid('getColumnFields'));
-                    for(var i=0; i<fields.length; i++){
-                        var col = $(this).datagrid('getColumnOption', fields[i]);
-                        col.editor1 = col.editor;
-                        if (fields[i] != param.field){
-                            col.editor = null;
-                        }
-                    }
-                    $(this).datagrid('beginEdit', param.index);
-                    var ed = $(this).datagrid('getEditor', param);
-                    if (ed){
-                        if ($(ed.target).hasClass('textbox-f')){
-                            $(ed.target).textbox('textbox').focus();
-                        } else {
-                            $(ed.target).focus();
-                        }
-                    }
-                    for(var i=0; i<fields.length; i++){
-                        var col = $(this).datagrid('getColumnOption', fields[i]);
-                        col.editor = col.editor1;
-                    }
-                });
-            },
-            enableCellEditing: function(jq){
-                return jq.each(function(){
-                    var dg = $(this);
-                    var opts = dg.datagrid('options');
-                    opts.oldOnClickCell = opts.onClickCell;
-                    opts.onClickCell = function(index, field){
-                        if (opts.editIndex != undefined){
-                            if (dg.datagrid('validateRow', opts.editIndex)){
-                                dg.datagrid('endEdit', opts.editIndex);
-                                opts.editIndex = undefined;
-                            } else {
-                                return;
-                            }
-                        }
-                        dg.datagrid('selectRow', index).datagrid('editCell', {
-                            index: index,
-                            field: field
-                        });
-                        opts.editIndex = index;
-                        opts.oldOnClickCell.call(this, index, field);
-                    }
-                });
-            }
-        });
+
+        var editIndex=-1;
+        var append=function(){
+
+                $('#drugdetailmanagerpanel').datagrid('appendRow',{});
+                editIndex = $('#drugdetailmanagerpanel').datagrid('getRows').length-1;
+                $('#drugdetailmanagerpanel').datagrid('selectRow', editIndex)
+                    .datagrid('beginEdit', editIndex);
+        };
 
 
         var accept=function (){
@@ -113,6 +71,9 @@ define(function () {
              sortOrder:'desc',*/
             fit:true,
             toolbar:'#drugdetailpaneltb',
+            onClickRow:function(index,row){
+                if(index!=editIndex)$('#drugdetailmanagerpanel').datagrid('endEdit',editIndex);
+            },
             pagination:true,
             pageSize:10,
             onBeforeLoad: function (params) {
@@ -129,6 +90,7 @@ define(function () {
         $('#drugdetailmanagerpanel').datagrid('enableCellEditing');
 
         $('#drugdetailpaneltb .save').click(accept);
+        $('#drugdetailpaneltb .append').click(append);
 
     }
 
